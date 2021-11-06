199ca4137e7459831a9ea1c01eaf5416da2c05c9
yikegu0215 <yikegu0215@gmail.com>
2021-11-05 21:38:17 -0700

    11/3

--- main/moBanMen/TreeTraversal.java
+++ main/moBanMen/TreeTraversal.java
@@ -1,4 +1,30 @@
 package moBanMen;
 
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class TreeTraversal {
+    //Iterative Inorder Traversal
+    public List<Integer> inOrderIterative(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        while(root != null || stack.isEmpty()) {
+            while(root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            //Operation
+            result.add(root.val);
+            //
+            root = root.right;
+        }
+        return result;
+
+    }
 }
